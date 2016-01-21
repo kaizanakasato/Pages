afa08b0863200a4b6a6ea51d045fd872a1b6315c
Y-Takuma <yse.iss.nakagame@gmail.com>
2016-01-21 12:10:40 +0900

    Make LoginSession 2

--- login.php
+++ login.php
@@ -1,13 +1,32 @@
 <?php
 	session_start();
 
-	$email = htmlspecialchars($_POST['email'], ENT_QUOTES);
-	$passwd = htmlspecialchars($_POST['passwd'], ENT_QUOTES);
+	// ログインボタンが押された
+	if($_POST['login']){
+		$email = htmlspecialchars($_POST['email'], ENT_QUOTES);
+		$passwd = htmlspecialchars($_POST['passwd'], ENT_QUOTES);
+		
+		if(($email == login_user) && ($passwd == login_pass)){
+			$_SESSION['login'] = 1;
+			header('Location: top.php');
+		}
+		else{
+			$_SESSION['error'] = 21;
+			header('Location: not.html');
+		}
+	}
 	
-	if(($email == login_user) && ($passwd == login_pass)){
-		$_SESSION['login'] = 1;
+	// ゲストボタンが押された
+	else if($_POST['guest']){
+		$email = 'guest@guest.com';
+		$passwd = 'guest';
+		$_SESSION['login'] = 2;
 		header('Location: top.php');
 	}
-	else
-		header('Location: not.html');
+	
+	// 例外
+	else{
+		$_SESSION['login'] = 3;
+		header('Location: not.php');
+	}
 ?>
